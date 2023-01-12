6ff4ff17fc48411a3a03ce512660b813faf7939b
christelle flores <57645973+christelleflores@users.noreply.github.com>
2023-01-11 22:16:05 -0500

    added image and motion

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import { MotionPlugin } from '@vueuse/motion'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(MotionPlugin)
+
+app.mount('#app')
